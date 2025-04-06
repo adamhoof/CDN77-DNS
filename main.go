34ba591fa8de4701520dd6cd02d33b4b5fa6a007
adamhoof <adam.hofman1@gmail.com>
2025-04-06 16:40:18 +0200

    Add routing data test to main

--- main.go
+++ main.go
@@ -12,8 +12,16 @@ func main() {
 		fmt.Println(err)
 	}
 
+	d := NewData()
+	err = d.LoadRoutingData("routing-data.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	_, testNet, _ := net.ParseCIDR("2001:49f0:d0b8:aaaa::/56")
 	pop, scpl := nd.Route(testNet)
+
+	pop, scpl = d.Route(testNet)
 
 	fmt.Printf("Pop: %+v, Scope prefix length: %+v \n", pop, scpl)
 }
